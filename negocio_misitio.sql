c08b8c5dc932b01741351ab8199af8b22df25c03
BraianFG <55467665+BraianFG@users.noreply.github.com>
2021-12-15 00:00:48 -0300

    Add files via upload

---
+++ negocio_misitio.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 15-12-2021 a las 02:57:22
+-- Versión del servidor: 10.5.12-MariaDB-cll-lve
+-- Versión de PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `u819747409_elforastero`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `negocio_misitio`
+--
+
+CREATE TABLE `negocio_misitio` (
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `favicon` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagen_sitio` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `negocio_misitio`
+--
+
+INSERT INTO `negocio_misitio` (`title`, `descripcion`, `favicon`, `imagen_sitio`) VALUES
+('El Forastero  ', 'Puedes comprarlo de forma fácil y rápida desde la comodidad de tu casa o donde estés, te lo enviaremos y entraremos en la web para realizar la compra.', '/images/logo.ico ', '/images/logo.jpg');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `negocio_misitio`
+--
+ALTER TABLE `negocio_misitio`
+  ADD UNIQUE KEY `title` (`title`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ usuarios.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 15-12-2021 a las 02:57:39
+-- Versión del servidor: 10.5.12-MariaDB-cll-lve
+-- Versión de PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `u819747409_elforastero`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ciudad` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `codPostal` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `celular` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `direccion`, `ciudad`, `codPostal`, `celular`, `email`, `password`) VALUES
+(346, 'Braian', 'Frediani Guelffi', ' Junin 559', 'Chivilcoy', '6620', '2346338767', 'admin@admin.com', '$2y$08$EpPVTv4NEm5MmT8r.hgPkeSQmh1JY/JaGE0t9kjQ/E2S4ZQypOUJW'),
+(355, 'Roberto ', 'Frediani ', 'Av Urquiza 541', 'Chivilcoy ', '', '2346521865', 'robertofrediani22@gmail.com', '$2y$08$yrs035uq81E5X2r9DQplhO1lHq0nvwgAQcv26G3Mkt2f/rftoWnda');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1003;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
