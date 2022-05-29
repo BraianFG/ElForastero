16caee4e870dbd05d5dc159f394d110288ae2b80
BraianFG <55467665+BraianFG@users.noreply.github.com>
2022-05-28 21:47:20 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php  
-  $servername = "localhost";
-  $database = "u819747409_braianfg287";
-  $username = "u819747409_braianfg287";
-  $password = "123456789Abc";
+  $servername = $_ENV['localhost'];
+  $database = $_ENV['u819747409_braianfg287'];
+  $username = "$_ENV['u819747409_braianfg287']";
+  $password = "$_ENV['123456789Abc'];
    
   $conn = mysqli_connect($servername, $username, $password, $database);
  
