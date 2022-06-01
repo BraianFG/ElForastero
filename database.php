2f17ac17045114d7fdb125b649a9e4683251be1b
BraianFG <55467665+BraianFG@users.noreply.github.com>
2022-05-31 22:08:16 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,12 @@
-<?php  
+<?php 
+    require('../vendorR/vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__,'.env');
+    $dotenv ->load();
+  
   $servername = $_ENV['DBHOST'];
   $database = $_ENV['DBNAME'];
-  $username = "$_ENV['DBUSER']";
-  $password = "$_ENV['DBPASS'];
+  $username = $_ENV['DBUSER'];
+  $password = $_ENV['DBPASS'];
    
   $conn = mysqli_connect($servername, $username, $password, $database);
  
