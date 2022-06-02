12841bf271e6a5cb4f75be3c63a2bc022b0b62fe
BraianFG <55467665+BraianFG@users.noreply.github.com>
2022-06-01 22:31:24 -0300

    Delete admin/admin directory

--- admin/admin/assets/js/Google-Analytics.php
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q9JRHDYJ16"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-Q9JRHDYJ16');
-</script>
--- admin/admin/slider/up.php
+++
@@ -1,4 +0,0 @@
- <div class="telegram">
-                <a href="#" id="boton-flotante" title="">  <img src="https://cdn.pixabay.com/photo/2013/07/12/13/43/arrow-147174_960_720.png" alt="Telegram" />
-                </a>
-</div>
