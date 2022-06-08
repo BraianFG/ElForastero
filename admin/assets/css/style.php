73de6c2c41d1b63ec76944e9a65809ad29f2e149
BraianFG <55467665+BraianFG@users.noreply.github.com>
2022-06-07 22:31:35 -0300

    Delete admin directory

--- admin/assets/css/style.php
+++
@@ -1,9 +0,0 @@
-<style>
-    <?php include 'assets/css/bootstrap/css/bootstrap.php' ?>
-     <?php include 'assets/css/uikit/css/uikit.php' ?>
-     <?php include 'assets/css/googlefonts/fonts.php'?>
-     <?php include "assets/js/alertify/css/alertify.min.css" ?>
-     <?php include "assets/js/alertify/css/alertify-1.min.css"?>
-     <?php include "assets/js/alertify/css/alertify-2.min.css" ?>
-</style>
-  
--- admin/assets/php/productos/botones.php
+++
@@ -1,1 +0,0 @@
-<a class="button primary CARRITO" > <i class="fas fa-cart-arrow-down fa-2x"></i> Agregar al carrito</a>
