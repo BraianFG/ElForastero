4c8890122dfd3c80cae8d806de93aac7c0b5536a
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-01-17 10:18:45 -0500

    Add files via upload

---
+++ Base de datos/u819747409_elforastero.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 17-01-2023 a las 15:17:43
+-- Versión del servidor: 10.5.12-MariaDB-cll-lve
+-- Versión de PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `u819747409_elforastero`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.admin: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'admin'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.carrito: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'carrito'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.categorias: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'categorias'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estilos`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.estilos: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'estilos'
+-- Error leyendo datos de la tabla u819747409_elforastero.estilos: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`estilos`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidad`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.localidad: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'localidad'
+-- Error leyendo datos de la tabla u819747409_elforastero.localidad: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`localidad`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.mensajes: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'mensajes'
+-- Error leyendo datos de la tabla u819747409_elforastero.mensajes: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`mensajes`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `MercadoPago`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.MercadoPago: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'MercadoPago'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `negocio`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.negocio: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'negocio'
+-- Error leyendo datos de la tabla u819747409_elforastero.negocio: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`negocio`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `negocio_misitio`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.negocio_misitio: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'negocio_misitio'
+-- Error leyendo datos de la tabla u819747409_elforastero.negocio_misitio: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`negocio_misitio`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.pedidos: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'pedidos'
+-- Error leyendo datos de la tabla u819747409_elforastero.pedidos: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`pedidos`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.productos: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'productos'
+-- Error leyendo datos de la tabla u819747409_elforastero.productos: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`productos`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `totalCarrito`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.totalCarrito: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'totalCarrito'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `totalPedido`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.totalPedido: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'totalPedido'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transferencia`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.transferencia: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'transferencia'
+-- Error leyendo datos de la tabla u819747409_elforastero.transferencia: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`transferencia`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.usuarios: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'usuarios'
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_eliminados`
+--
+-- Error leyendo la estructura de la tabla u819747409_elforastero.usuarios_eliminados: #1142 - SHOW comando negado para usuario: 'u819747409_braianfg287'@'127.0.0.1' para tabla 'usuarios_eliminados'
+-- Error leyendo datos de la tabla u819747409_elforastero.usuarios_eliminados: #1064 - Algo está equivocado en su sintax cerca 'FROM `u819747409_elforastero`.`usuarios_eliminados`' en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
