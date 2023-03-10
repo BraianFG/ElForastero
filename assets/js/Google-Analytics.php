097622ec9befa82f1dcd87605d88aae7646611f5
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-03-10 18:54:39 -0500

    Delete assets directory

--- assets/js/Google-Analytics.php
+++
@@ -1,7 +0,0 @@
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script src="https://www.googletagmanager.com/gtag/js?id=G-Q9JRHDYJ16" defer></script>
-
-<script defer>
-  window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-Q9JRHDYJ16');
-</script>
