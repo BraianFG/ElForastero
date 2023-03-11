216698efb69added98f4a4bf8a8b49ec4fd0c740
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-03-11 18:05:14 -0500

    Update visitas.php

--- admin/visitas/visitas.php
+++ admin/visitas/visitas.php
@@ -7,13 +7,15 @@
                 <th>N°</th>
                 <th>IP</th>
                 <th>Página</th>
+                <th>País</th>
+                <th>Ciudad</th>
                 <th>Navegador</th>
                 <th>Referencia</th>
                 <th>Fecha</th>
                 <th>Hora</th>
               </tr>
         </thead>        
-      <?php $productosp1 = "SELECT  `fecha`,`ip`,`hora`,`num`,`pais`,`pagina`,`navegador`,`referido` FROM `visitas`  GROUP BY `ip`,`pagina` ORDER BY `fecha` , `hora` DESC;" ;     
+      <?php $productosp1 = "SELECT  `fecha`,`ip`,`hora`,`num`,`pais`,`ciudad`,`pagina`,`navegador`,`referido` FROM `visitas`  GROUP BY `ip`,`pagina` ORDER BY `fecha` , `hora` DESC;" ;     
           $result3 = mysqli_query($conn,$productosp1);
 
            while($mostrar_productosp1 = mysqli_fetch_array($result3)){
@@ -23,6 +25,8 @@
                    <td><?php echo $mostrar_productosp1["num"] ?></td>
                    <td><?php echo $mostrar_productosp1["ip"] ?></td>
                    <td><?php echo $mostrar_productosp1["pagina"] ?></td>
+                   <td><?php echo $mostrar_productosp1["pais"] ?></td>
+                   <td><?php echo $mostrar_productosp1["ciudad"] ?></td>
                    <td><?php echo $mostrar_productosp1["navegador"] ?></td>
                    <td><?php echo $mostrar_productosp1["referido"] ?></td>
                    <td><?php echo $mostrar_productosp1["fecha"] ?></td>
