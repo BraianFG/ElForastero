2012a3a623f705789cd7bb9eb5fb4ad40e9efae4
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-03-14 14:20:05 -0400

    Update visitas.php

--- admin/visitas/visitas.php
+++ admin/visitas/visitas.php
@@ -1,4 +1,9 @@
- <div id="usuarios" class="uk-container  uk-container-large wrap">
+ <div id="visitas" class="uk-container  uk-container-large wrap">
+    <ul class="lista">
+        <li class="lista__item">
+                 <a class="button primary" href="../admin" >Volver</a>
+        </li>
+    </ul>
       <header class="caption"> <i class="fas fa-users"> </i>Visitas</header>
           <div class="uk-overflow-auto">    
              <table id="user" class="uk-table uk-table-striped ">
@@ -7,12 +12,17 @@
                 <th>N°</th>
                 <th>IP</th>
                 <th>Página</th>
+                <th>País</th>
+                <th>Ciudad</th>
                 <th>Navegador</th>
+                <th>Sistema operativo</th>
+                <th>Dispositivo</th>
+                <th>Referencia</th>
                 <th>Fecha</th>
                 <th>Hora</th>
               </tr>
         </thead>        
-      <?php $productosp1 = 'SELECT * FROM `visitas` ' ;     
+      <?php $productosp1 = "SELECT  `fecha`,`ip`,`hora`,`num`,`pais`,`ciudad`,`pagina`,`navegador`,`referido`,`dispositivo`,`sistema_operativo` FROM `visitas`  GROUP BY `ip`,`pagina` ORDER BY `fecha` , `hora` DESC;" ;     
           $result3 = mysqli_query($conn,$productosp1);
 
            while($mostrar_productosp1 = mysqli_fetch_array($result3)){
@@ -22,7 +32,12 @@
                    <td><?php echo $mostrar_productosp1["num"] ?></td>
                    <td><?php echo $mostrar_productosp1["ip"] ?></td>
                    <td><?php echo $mostrar_productosp1["pagina"] ?></td>
+                   <td><?php echo $mostrar_productosp1["pais"] ?></td>
+                   <td><?php echo $mostrar_productosp1["ciudad"] ?></td>
                    <td><?php echo $mostrar_productosp1["navegador"] ?></td>
+                   <td><?php echo $mostrar_productosp1["sistema_operativo"] ?></td>
+                   <td><?php echo $mostrar_productosp1["dispositivo"] ?></td>
+                   <td><?php echo $mostrar_productosp1["referido"] ?></td>
                    <td><?php echo $mostrar_productosp1["fecha"] ?></td>
                    <td><?php echo $mostrar_productosp1["hora"] ?></td>
               </tr>
@@ -34,9 +49,9 @@
  }
 ?>  
             </table>
-
    </div>
 </div>
+</script>
 
 
 
