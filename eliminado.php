90e6fc3fd4b93b81c72931761f518c32c35f0d50
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-03-14 14:21:32 -0400

    Update eliminado.php

--- eliminado.php
+++ eliminado.php
@@ -1,1 +1,61 @@
-<h2>Fuiste eliminado</h2>
+<html>
+    <head>
+         <title>Fuiste bloqueado</title>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+         
+         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+        
+            <meta charset="utf-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+<body>
+    <div class="color text-center">     
+        <div class="container-md container">    
+        <img src="https://cdn.pixabay.com/photo/2018/01/04/15/51/404-error-3060993__340.png" width="280"/>
+            <h2 class="texto">Fuiste bloqueado</h2>
+                            <a href="/ingresar" class="button">Volver al inicio</a>
+        </div>    
+    </div>  
+<style>
+    body{
+       background-color:#F4F6F6;
+       font-size:16px;
+    }
+    .color{
+        color:#E67E22;
+        font-weight:bold;
+        text-transform:uppercase;
+        padding:2em 0;
+        height:100%;
+    }
+    
+    .texto{
+        font-size:2em;
+        margin:1em 0 1em 0;
+        max-width:50em;
+        margin-bottom:2em;
+        color:#641E16;
+    }
+    
+    .button{
+        background-color:#641E16;
+        color:#FEFEFE;
+        font-weight:bold;
+        text-transform:uppercase;
+        width:100%;
+        text-decoration:none;
+        padding:2em 4em;
+        border-radius:0.8em;
+    }
+    
+        .button:hover{
+                  color:#FEFEFE;
+        }
+        
+   svg{
+      margin-left:0.5em; 
+   }        
+</style>
+ </body>
+</html>
