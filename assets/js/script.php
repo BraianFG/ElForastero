d5efbcf74f04c8bf0cc4ce3649a3d9f3dc660bf3
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-10-14 14:39:43 -0300

    Update script.php

--- assets/js/script.php
+++ assets/js/script.php
@@ -19,5 +19,3 @@
 <?php include 'assets/css/uikit/js/uikit.php'?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==" crossorigin="anonymous" referrerpolicy="no-referrer" defer ></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" defer></script>
