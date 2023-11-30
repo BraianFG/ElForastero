276dfc2adfbd741c447f2375620cc7e76679583f
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-11-30 07:45:39 -0300

    Update registrarse.php

--- registrarse.php
+++ registrarse.php
@@ -15,7 +15,7 @@
       <?php include 'assets/css/main.php' ?>
        <?php include 'assets/js/script.php'?>
       <?php include "assets/css/style.php" ?>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
 </head>    
 <body>
     <?php include "visitas.php" ?>
@@ -83,6 +83,9 @@
     </div>
                   <div class="uk-card uk-card-default uk-card-body">
          <p class="uk-text-center">¿Ya tenés una cuenta? ,<br> <a class="marron" href="ingresar">Solo tenés que ingresar para comenzar a comprar </a></a></p>
+        </div>
+        <div id="resultado__registro">
+            
         </div>
 </div>
 </div>
