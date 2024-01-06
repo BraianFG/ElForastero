eedf5c98f2ec6252b351bd7e147f75cc0903f384
BraianFG <55467665+BraianFG@users.noreply.github.com>
2024-01-06 20:51:43 -0300

    Add files via upload

--- assets/js/script.php
+++ assets/js/script.php
@@ -1,20 +1,9 @@
 
-
- <!---Font Awesome icons--->
-<!---
-<script src="assets/js/font-awesome/all.js" defer></script>
-<script src="assets/js/font-awesome/brands.min.js" defer></script>
-<---
-
   <!--Alertify -->
   <script src="assets/js/alertify/js/alertify.min.js" defer></script>
   
  <! --Ajax--> 
-  <script src="assets/js/ajax/ajax.js" defer></script>
-  
-
-<!--Lazysizes -->
-<script "assets/js/lazysizes/lazysizes.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
 
 <!-- Uikit -->
 <?php include 'assets/css/uikit/js/uikit.php'?>
