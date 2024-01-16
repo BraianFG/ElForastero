d628c6a5ffe577591a035df868575404e5a5b963
BraianFG <55467665+BraianFG@users.noreply.github.com>
2024-01-16 17:41:36 -0300

    Delete assets directory

--- assets/js/script.php
+++
@@ -1,9 +0,0 @@
-<!--JQUERY-->
-<script src="assets/js/JQuery/jquery-3.6.0.js" async></script>
-<script src="assets/js/JQuery/jquery-3.6.0.min.js" async></script>
-
- <!--UI kit -->
- <script src="assets/css/uikit/js/uikit.js" async></script>
-
-<!--alertify -->
-<script src="assets/js/alertify/js/alertify.min.js" async></script>
--- assets/php/salir.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   session_start();
-   unset($_SESSION['id']);
-   if(!isset($_SESSION['id'])){
-     echo '<script>location.href="../../ingresar";</script>'; 
-   }
- 
-?>   
