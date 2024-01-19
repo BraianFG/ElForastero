1842fee59e51e10cd730e98ab0a5fca3e7544244
BraianFG <55467665+BraianFG@users.noreply.github.com>
2024-01-19 19:59:02 -0300

    Delete datos directory

--- datos/eliminar_datos.php
+++
@@ -1,5 +0,0 @@
-<?php
-   $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
-   
-   unlink("datos-$id.txt");
-?>
--- datos/eliminar_datos3.php
+++
@@ -1,4 +0,0 @@
-<?php
-   $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
-    unlink("datos3-$id.txt");
-?>
