4ed7a57cbab516596d8bdf9f87aae40510722b0e
BraianFG <55467665+BraianFG@users.noreply.github.com>
2024-01-20 14:17:29 -0300

    Delete top-cache.php

--- top-cache.php
+++
@@ -1,1 +0,0 @@
-<?php $url = $_SERVER["SCRIPT_NAME"]; $break = explode('/', $url); $file = $break[count($break) - 1]; $cachefile = 'cached-'.substr_replace($file ,"",-4).'.html'; $cachetime = 18000; ?>
