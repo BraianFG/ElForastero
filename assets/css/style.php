9f75593f6dc109b3e3c0e435b8ca304cff06de87
BraianFG <55467665+BraianFG@users.noreply.github.com>
2024-01-20 16:26:36 -0300

    Update style.php

--- assets/css/style.php
+++ assets/css/style.php
@@ -1,9 +1,14 @@
 <style>
-    <?php include 'assets/css/bootstrap/css/bootstrap.php' ?>
-     <?php include 'assets/css/uikit/css/uikit.min.css' ?>
+ 
      <?php include "assets/js/alertify/css/alertify.min.css" ?>
      <?php include "assets/js/alertify/css/alertify-1.min.css"?>
      <?php include "assets/js/alertify/css/alertify-2.min.css" ?>
     </style>     
 
 <?php include "assets/css/icons.php" ?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit-icons.min.js" defer></script>
